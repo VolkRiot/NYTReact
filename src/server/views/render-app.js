204c87f9b8978e292e64278b22610b490eded76a
Mikhail Metrikin <metrikin@gmail.com>
2017-06-11 22:55:19 -0700

    Minimal thememing from Bootstrap

--- src/server/views/render-app.js
+++ src/server/views/render-app.js
@@ -3,10 +3,23 @@ const renderApp = title =>
 <html>
   <head>
     <title>${title}</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/slate/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-RpX8okQqCyUNG7PlOYNybyJXYTtGQH+7rIKiVvg1DLg6jahLEk47VvpUyS+E2/uJ"
+    crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-    <div id="app"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="app"></div>
+        </div>
+      </div>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+    crossorigin="anonymous"></script>
     <script src="http://localhost:3000/public/js/bundle.js"></script>
   </body>
 </html>
