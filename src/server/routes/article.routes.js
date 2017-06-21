80bbe73a4f4a9dafe0e2b510e6d30f4bd0bd9248
Mikhail Metrikin <metrikin@gmail.com>
2017-06-20 23:39:13 -0700

    Alternate branch pre-commit

--- src/server/routes/article.routes.js
+++ src/server/routes/article.routes.js
@@ -5,8 +5,11 @@ const Router = express.Router();
 
 Router.get('/saved', (req, res) => {
   Article.find({}).exec((err, doc) => {
-    if (err) res.status(500).send('Error while retrieving your durned Articles');
-    res.status(200).send(doc);
+    if (err) {
+      res.status(500).send('Error while retrieving your durned Articles');
+    } else {
+      res.status(200).send(doc);
+    }
   });
 });
 
@@ -23,8 +26,11 @@ Router.post('/saved', (req, res) => {
 
 Router.delete('/saved', (req, res) => {
   Article.findByIdAndRemove(req.query.id).then((resp, err) => {
-    if (err) res.status(500).send('Failed to remove this record');
-    res.status(200).send({ success: true });
+    if (err) {
+      res.status(500).send('Failed to remove this record');
+    } else {
+      res.status(200).send({ success: true });
+    }
   });
 });
 
