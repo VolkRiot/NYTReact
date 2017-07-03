3f9eb66ca0a43cfc69d63efe5f8e826f48faf8e5
Mikhail Metrikin <metrikin@gmail.com>
2017-07-03 14:59:47 -0700

    Webpack convert to production

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('development'),
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
     new webpack.optimize.UglifyJsPlugin(),
